831d02213cc62f464eb89ecf1b44db6d27178c4d
Ksenia06041991 <118802364+Ksenia06041991@users.noreply.github.com>
2022-12-23 02:32:55 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа  
+//и выдаёт максимальное из этих чисел. 
+ 
+//2, 3, 7 -> 7 
+//44 5 78 -> 78 
+//22 3 9 -> 22 
+ 
+ 
+Console.Write("Введите число а:");  
+int a = int.Parse(Console.ReadLine()!);  
+Console.Write("Введите число b:");  
+int b = int.Parse(Console.ReadLine()!);  
+Console.Write("Введите число c:");  
+int c = int.Parse(Console.ReadLine()!);  
+ 
+if (a > b && a > c) 
+                    Console.WriteLine("Nubmer Max = " + a);  
+                else if (b > c) 
+                    Console.WriteLine("Number Max = " + b);  
+                else 
+                    Console.WriteLine("Number Max = " + c);  
+             
+            Console.ReadLine();
